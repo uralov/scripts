bddb39e19391e42f60e90f7cee886c73671fffc5
Alexander Uralov <yralov87@gmail.com>
2018-08-13 13:42:16 +0300

    fixed gz extention for backup_pg.sh script

--- backup_pg.sh
+++ backup_pg.sh
@@ -6,8 +6,12 @@ mkdir -p $backup_dir/$date
 
 for dbname in `sudo -u postgres psql -t -c "SELECT datname FROM pg_database WHERE datistemplate = false;"`; do
     echo "Dump $dbname..."
-    sudo -u postgres pg_dump -Fc -x -O $dbname | gzip > $backup_dir/$date/$dbname.sql
+    sudo -u postgres pg_dump -x -O $dbname | gzip > $backup_dir/$date/$dbname.sql.gz
 done;
 
 # Удаляем файлы старше 30 дней
 find $backup_dir -atime +30 | xargs rm -rf
+
+
+
+
